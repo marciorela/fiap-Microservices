2ebe13e95d96e9eb357bec90068d8e66b43855f1
Marcio Rela <marciorela@hotmail.com>
2022-06-08 22:08:58 -0300

    Atividade 1

---
+++ GeekBurger.Products/GeekBurger.Products.Contract/DTOs/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekBurger.Products.Contract.DTOs
+{
+    public class Product
+    {
+        public string StoreName { get; set; }
+        public Guid ProductId { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public List<Item> Items { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class Item
+    {
+        public Guid ItemId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ GeekBurger.Products/GeekBurger.Products.Contract/DTOs/ProductChanged.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekBurger.Products.Contract.DTOs
+{
+    public class ProductChanged
+    {
+        public ProductState State { get; set; }
+        public Product Product { get; set; }
+    }
+
+    public enum ProductState
+    {
+        Deleted = 2,
+        Modified = 3,
+        Added = 4
+    }
+}
